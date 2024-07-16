a445636c28c1016d4728adaed9768afa5cca5621
T-Chol <taban.chol@student.moringaschool.com>
2024-07-16 19:07:58 +0300

    Solves Arrow-Function

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+let a=200,b=100;
+
+// Function expression
+const divide = function(a, b) {
+    return a / b;
+};
+
+// Arrow function
+const square = (x) => {
+    return x * x;
+};
+// Arrow function
+const add = (a, b) => {
+    return a + b;
+};
